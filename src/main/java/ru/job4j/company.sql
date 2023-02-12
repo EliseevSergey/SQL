6cbb737e0a1b4c4c481556e4f8ddd5060c2c1d5c
EliseevSergey <s.eliseev1@gmail.com>
2023-02-12 16:23:49 +0300

    1. Схема прав пользователи и роли. [#1093 #390665]

--- src/main/java/ru/job4j/company.sql
+++ src/main/java/ru/job4j/company.sql
@@ -10,8 +10,6 @@ insert into roles(id, title) values(20, 'engineer');
 insert into roles(id, title) values(30, 'sn_engineer');
 insert into roles(id, title) values(40, 'director');
 
-select * from roles;
-
 create table users (
 	id INT primary key,
 	name TEXT,
@@ -24,17 +22,15 @@ insert into users(id, name, role_id) values (3, 'Ivanov', 20);
 insert into users(id, name, role_id) values (4, 'Vasilev', 30);
 insert into users(id, name, role_id) values (5, 'Romashkin', 40);
 
-select * from users;
-
 create table rules (
 	id INT primary key,
 	level TEXT
 );
+
 insert into rules (id, level) values (0, 'highest');
 insert into rules (id, level) values (1, 'high');
 insert into rules (id, level) values (2, 'midlle');
 insert into rules (id, level) values (3, 'low');
-select * from rules;
 
 create table roles_rules (
 	id INT primary key,
@@ -46,32 +42,6 @@ insert into roles_rules (id, role_id, rule_id) values(1, 10, 3);
 insert into roles_rules (id, role_id, rule_id) values (2, 40, 0);
 
 select * from roles_rules;
-
-create table comments (
-	id INT primary key references items(comment_id),
-	content TEXT,
-	likes INT
-);
-
-create table items (
-	id INT primary key,
-	state_type TEXT references state(type),
-	category_id INT references category(id),
-	user_id INT references users(id),
-	content TEXT,
-	attach_id INT UNIQUE,
-	comment_id INT UNIQUE
-);
-
-insert into items (id, state_type, category_id, user_id, content, attach_id, comment_id)
-values			  (1, 'on schedule', 2, 1, 'db creation', 2, 34);
-insert into items (id, state_type, category_id, user_id, content, attach_id, comment_id)
-values			  (2, 'delayed', 1, 5, 'endless meetings', 1, 33);
-
-insert into comments(id, content, likes) values (33, 'faster', 666);
-insert into comments(id, content, likes) values (34, 'ahead of schedule', 777);
-select * from comments;
-
 
 create table state (
 	type TEXT primary key
@@ -91,11 +61,38 @@ insert into category (id, name) values (2, 'normal');
 insert into category (id, name) values (3, 'canceled');
 select * from category;
 
+create table items (
+	id INT primary key,
+	state_type TEXT references state(type),
+	category_id INT references category(id),
+	user_id INT references users(id),
+	content TEXT,
+	attach_id INT UNIQUE,
+	comment_id INT UNIQUE
+);
+
+insert into items (id, state_type, category_id, user_id, content, attach_id, comment_id)
+values			  (1, 'on schedule', 2, 1, 'db creation', 2, 34);
+insert into items (id, state_type, category_id, user_id, content, attach_id, comment_id)
+values			  (2, 'delayed', 1, 5, 'endless meetings', 1, 33);
+
+create table comments (
+	id INT primary key,
+	items_id INT references items(comment_id),
+	content TEXT,
+	likes INT
+);
+
+insert into comments(id,items_id, content, likes) values (1,33, 'faster', 666);
+insert into comments(id, items_id, content, likes) values (2, 34,'ahead of schedule', 777);
+select * from comments;
+
 create table attachs (
-	id INT references items(attach_id) primary key,
+	id INT primary key,
+	items_id INT references items(attach_id),
 	name TEXT,
 	size INT
 );
 
-insert into attachs(id, name, size) values (1, 'coffee_spil', 100);
-insert into attachs(id, name, size) values (2, 'AI', 100);
+insert into attachs(id, items_id, name, size) values (1, 1, 'coffee_spil', 100);
+insert into attachs(id, items_id, name, size) values (2, 2, 'AI', 100);
