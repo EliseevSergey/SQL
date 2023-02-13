48778b219fd8bfa863bb7114269c490869de630e
EliseevSergey <s.eliseev1@gmail.com>
2023-02-13 23:25:01 +0300

    1. Inner join [#373617]

--- src/main/java/ru/job4j/Pirozhkovaya.sql
+++ src/main/java/ru/job4j/Pirozhkovaya.sql
@@ -1,18 +1,18 @@
-create table pirozhki(
-id serial primary key,
-	title CHAR(100),
-	weight SMALLINT,
-	vegan BOOL,
-	price SMALLINT,
-	prod_date DATE
-);
-insert into pirozhki(title, weight, vegan, price, prod_date) 
-values('Energon', 300, false, 100, '2023-02-08');
-insert into pirozhki(title, weight, vegan, price, prod_date) 
-values('GreenPiace', 200, true, 50, '2023-02-07');
-insert into pirozhki(title, weight, vegan, price, prod_date) 
-values('Kanibal', 400, false, 200, '2023-01-01');
-update pirozhki set price = 666;
-select * from pirozhki;
-delete from pirozhki;
-select * from pirozhki;
+values(3, 'Kanibal', 400, false, 200, 4, '2023-01-01');
+insert into pirozhki(id, title, weight, vegan, price, baker_id, sop)
+values(4, 'Syogun', 600, false, 200, 3, '2023-01-01');
+insert into pirozhki(id, title, weight, vegan, price, baker_id, sop)
+values(5,'Shawarma', 600, false, 200, 5, '2022-01-01');
+
+--no alliance--
+select * from pirozhki inner join bakers on pirozhki.baker_id = bakers.id;
+
+--alliances--
+select p.title as Пирог, b.surname as Пекарь from pirozhki as
+p join bakers as b on p.baker_id = b.id;
+
+select p.title as Пирог, b.surname as Пекарь, p.price as Цена from pirozhki as
+p join bakers as b on p.baker_id = b.id;
+
+select b.surname Пекарь, b.country Страна, p.sop as "Старт Продаж" from pirozhki as
+p join bakers as b on p.baker_id = b.id;
