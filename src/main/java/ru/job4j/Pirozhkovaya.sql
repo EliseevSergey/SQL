4be56dae4d5672f324ebdb2cccac46f0891ab0d8
Sergey Eliseev <s.eliseev1@gmail.com>
2023-03-02 09:23:53 +0300

    upd

--- src/main/java/ru/job4j/Pirozhkovaya.sql
+++ src/main/java/ru/job4j/Pirozhkovaya.sql
@@ -31,12 +31,14 @@ insert into pirozhki(id, title, weight, vegan, price, baker_id, sop)
 values(4, 'Syogun', 600, false, 200, 3, '2023-01-01');
 insert into pirozhki(id, title, weight, vegan, price, baker_id, sop)
 values(5,'Shawarma', 600, false, 200, 5, '2022-01-01');
+
 select * from pirozhki;
 
 create table clients(
     id serial primary key,
     name TEXT
 );
+drop table clients cascade;
 
 insert into clients (name) values ('Megan Fox');
 insert into clients (name) values ('Greta Thunberg');
@@ -52,21 +54,20 @@ create table po(
     date DATE
 );
 
+drop table po cascade;
 create table order_list (
     order_list_id INT references po(id),
     pirozhok_id INT references pirozhki(id),
     qty INT
 );
 
-select * from clients;
-drop table clients CASCADE;
-drop table po;
-
 insert into po (client_id, date) values (1, '2023-01-01');
 insert into po (client_id, date) values (2, '2023-01-01');
 insert into po (client_id, date) values (3, '2023-01-02');
 insert into po (client_id, date) values (4, '2023-01-02');
 insert into po (client_id, date) values (5, '2023-01-02');
+
+select * from po;
 
 select *from order_list;
 insert into order_list (order_list_id, pirozhok_id, qty) values (1, 1, 4);
@@ -79,30 +80,15 @@ insert into order_list (order_list_id, pirozhok_id, qty) values (4, 4, 3);
 insert into order_list (order_list_id, pirozhok_id, qty) values (4, 5, 4);
 insert into order_list (order_list_id, pirozhok_id, qty) values (5, 5, 2);
 
-select p.id as po_id, date, p.client_id as client_id, c.name as customer,  p.pirozhok_id, pi.title as pirozhok
-, p.qty as qty_pirozhkov, b.surname as baker from po as p
-join clients as c
-on c.id = p.client_id
-join pirozhki as pi
-on pi.id = p.pirozhok_id
-join bakers as b
-on pi.baker_id = b.id
-join order_list as ol
-on ol.order_list_id = p.id;
-
 --TOTAL NEW
 select po.id as po_id, date, po.client_id as client_id, c.name as customer
-, ol.pirozhok_id, p.title, p.price as "price for 1",  ol.qty, p.price * ol.qty as "TOTAL"
-
+, ol.pirozhok_id, p.title, p.price as "price for 1",  ol.qty, p.price * ol.qty as "TOTAL",
+b.id as baker_id, b.surname as baker
 from po as po
- join clients as c
-on c.id = po.client_id
-join order_list as ol
-on ol.order_list_id = po.id
-join pirozhki as p
-on p.id = ol.pirozhok_id;
-
-
+join clients as c on c.id = po.client_id
+join order_list as ol on ol.order_list_id = po.id
+join pirozhki as p on p.id = ol.pirozhok_id
+join bakers as b on p.baker_id =b.id;
 
 --no alliance--
 select * from pirozhki inner join bakers on pirozhki.baker_id = bakers.id;
