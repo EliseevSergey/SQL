c7b3030e35d9fab7d01073af6e73df70cb8eab3a
Sergey Eliseev <s.eliseev1@gmail.com>
2023-04-17 13:25:38 +0300

    3. SQL Select by company [#2760]

--- src/main/java/ru/job4j/sql/corporation.sql
+++ src/main/java/ru/job4j/sql/corporation.sql
@@ -25,17 +25,20 @@ insert into person (id, name, company_id) values (1, 'Nakazawa-san', 1),(4, 'Kob
 (9, 'Pavel Georgevich', 6), (10, 'Alexander Valentinovich', 6),
 (12, 'Mr. Free', null);
 
-delete from company;
-delete from person;
-
---everybody + all companies
+--0. OUT OF TASK FROM JOB4J. Everybody + all companies
 select c.id as company_id , c.name, p.id as person_id, p.name, p.company_id as wokr_place_id
  from company as c full join person as p on c.id = p.company_id order by c.id ASC;
 
---without employees from company_id = 5
-select p.id as person_id, p.name, p.company_id as wokr_place_id, c.id as company_id , c.name
- from person as p left join company as c on p.company_id = c.id where p.company_id <> 5 order by p.id ASC;
+--1. Without employees from company_id = 5, but including unemployed
+select p.id as person_id, p.name, p.company_id as work_place_id, c.id as company_id , c.name
+ from person as p left join company as c on p.company_id = c.id where p.company_id <> 5 or p.company_id is null order by p.id ASC;
 
-
---
-select count(p.name), c.name, p.name, p.company_id from company as c join person as p on c.id = p.company_id group by c.id ;
+--2. Company list with maximum employees quantity
+select max_staff_qty, company
+ from (
+ select count(p.name) as max_staff_qty, c.name as company
+ from company as c join person as p on c.id = p.company_id
+ group by c.id
+ ) as subMax
+ where max_staff_qty = (select max(max_staff_qty) from (select count(p.name) as max_staff_qty
+  from company as c join person as p on c.id = p.company_id group by c.id) as subFilter);
